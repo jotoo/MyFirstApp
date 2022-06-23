9042e22ab88ab0318e14108395ce5713f60fd5b6
jotoo <odintmn2042@yandex.ru>
2022-06-23 16:32:42 +0500

    Test3

--- app/src/main/java/ru/jotoo/myfirstapp/MainActivity.java
+++ app/src/main/java/ru/jotoo/myfirstapp/MainActivity.java
@@ -3,12 +3,44 @@ package ru.jotoo.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.digit_comparator);
         setContentView(R.layout.activity_main);
+
+        /*EditText firstDigit = findViewById(R.id.first_digit);
+        EditText secondDigit = findViewById(R.id.second_digit);
+
+        Button compare = findViewById(R.id.compare);
+
+        TextView result = findViewById(R.id.result);
+
+        compare.setOnClickListener(v -> {
+            String txtFirstDigitValue = firstDigit.getText().toString();
+            String txtSecondDigitValue = secondDigit.getText().toString();
+
+            try {
+                Integer firstValue = Integer.valueOf(txtFirstDigitValue);
+                Integer secondValue = Integer.valueOf(txtSecondDigitValue);
+
+
+                if (firstValue.equals(secondValue)) {
+                    result.setText("Ecuals");
+                } else {
+                    result.setText("Not Ecual");
+                }
+            } catch (NumberFormatException e) {
+                result.setText("Error");
+            }
+
+        });*/
     }
 }
